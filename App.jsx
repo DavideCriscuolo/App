0fe4f0ca3564c25650be9fb10894399c5ae0b35f
DavideCriscuolo <davide.alcatel@gmail.com>
2025-11-02 17:30:12 +0100

    intsallato nativewind

--- App.jsx
+++ App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "react-native-gesture-handler";
 import "react-native-gesture-handler";
-
+import { Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screen/HomeScreen";
 import ProfileScreen from "./screen/ProfileScreen";
-
+import "./global.css";
 const Stack = createNativeStackNavigator();
 
 export default function App() {
